71f518d9619e43aaf7a1e87e16d7959f0f744a83
eunbin.choi <choiqls43@gmail.com>
2025-04-25 16:58:54 +0900

    vscode를 통한 두 번째 커밋

--- 250425/firts.py
+++ 250425/firts.py
@@ -4,5 +4,28 @@
 '''
 다중 주석 
 '''
-print("내일은 뭐먹지?")
-print("오늘 점심은 김치볶음밥")
+#변수 
+name = "김치찌개"  #변수 = 값 / name 코드가 끝나면 사라진다. 
+food = "김치볶음밥"
+# 변수이름은 있어야 한다. 
+
+money = "1000원"
+student = "홍길동"
+book_name = "나는 메트로폴리탄 미술관의 경비원입니다."
+brand = "샤넬"
+make = "한국"
+
+print(money, student, book_name, brand, make)
+a= 10
+b= 20
+print(a,"+",a,"=", b)
+
+PI=3.14
+PI= 3.14
+
+print(type(PI))
+print(type(name))
+
+
+
+
