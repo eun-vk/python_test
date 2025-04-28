2736277be8baa872c494cdc4223e889ff57a6d62
eunbin.choi <choiqls43@gmail.com>
2025-04-28 18:10:17 +0900

    250428_6

---
+++ 250428/second2.py
@@ -0,0 +1,35 @@
+# #건강 정보 프로그램 
+# print("키, 몸무게, 성별, 나이, 이름을 차례대로 입력해주세요.")
+# inf = input()
+# height, weight, gender, age, name = inf.split(' ')
+# print(f"입력된 정보\n키 : {height}\n몸무게: {weight}\n성별: {gender}\n나이: {age}\n이름: {name}" )
+
+# print("이름, 나이, 성별을 입력해주세요.")
+# inf = input()
+# name, age, gender = inf.split(" ")
+# print(f"사용자 정보:\n이름: {name}\n나이: {age}\n성별: {gender}")
+
+#입력 구현 
+# print("당신의 취미에 대해 알려주세요.")
+# hob = input()
+# b = hob*1
+
+#print(f"당신의 취미는 {hob}네요.\t저도 {hob}즐겨해요.\n{b}{b}다음에 같이 해봐요.")
+
+#10년 뒤 나이 예측하기 
+# print("10년뒤 나이를 알려드립니다. 현재 나이를 입력해주세요.")
+# age = int(input())
+# age1 = age + 10
+# print(f"당신의 10년 뒤 나이는 {age1}살 입니다.")
+
+#비밀번호 설정하기 
+password = "mypassword"
+user_input = input("비밀번호를 입력하세요:")
+
+if user_input ==password:
+    print("다음")
+
+else:
+    print("비밀번호가 틀렸습니다. 다시 입력해주세요.")
+
+password()
